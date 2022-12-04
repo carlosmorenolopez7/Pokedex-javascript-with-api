df1ad473feb64cd30906bc6dc6a5ef5b67963ecb
Carlos Moreno López <116076184+carlosmorenolopez7@users.noreply.github.com>
2022-12-04 20:39:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const PokemonContainer = document.getElementById("pokemon_containerID");
 const SearchContainer = document.getElementById("search_containerID");
 const SearchElement = document.createElement("input");
-const PokemonNumber = 151;
+const PokemonNumber = 905;
+var j = 20;
 SearchElement.setAttribute("type", "text");
 SearchElement.setAttribute("name", "searchBar");
 SearchContainer.appendChild(SearchElement);
+
+window.addEventListener('scroll',()=>{
+  if(window.scrollY + window.innerHeight >= 
+  document.documentElement.scrollHeight){
+    receivePokemons(false);
+    j += 20;
+  }
+})
 
 const createPokemonCard = (pokemon) => {
   const PokemonElement = document.createElement("div");
   const PokemonName = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
   const PokemonID = pokemon.id;
-  const PokemonType = pokemon.types[0].type.name;
+  const PokemonType1 = pokemon.types[0].type.name;
   PokemonElement.setAttribute("id", PokemonName);
   const PokemonTypeColors = {
     fire: "#EE8130",
@@ -32,17 +41,28 @@ const createPokemonCard = (pokemon) => {
     dark: "#705746",
     steel: "#B7B7CE",
   };
-  const AddColors = PokemonTypeColors[PokemonType];
+  const AddColors = PokemonTypeColors[PokemonType1];
   PokemonElement.style.backgroundColor = AddColors;
-  const PokemonInnerHTML = `
-    <div class="pokemon_imageContainer" id="${PokemonName}">
+  PokemonInnerHTML = `
+    <div class="pokemon__imageContainer" id="${PokemonName}">
       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${PokemonID}.png"/>
     </div>
-    <div class="pokemon_infomationContainer">
-      <span class="pokemon_id">#${PokemonID.toString()}</span>
-      <h3 class="pokemon_name">${PokemonName}</h3>
-      <small class="pokemon_type">Type: <span>${PokemonType}</span></small>
+    <div class="pokemon__infomationContainer">
+      <span class="pokemon__id">#${PokemonID.toString().padStart(3, "0")}</span>
+      <h3 class="pokemon__name">${PokemonName}</h3>
+      <small class="pokemon__type">Tipo: <span>${PokemonType1}</span></small><br>
     </div>`;
+  if(pokemon.types[1] != null){
+    const PokemonType2 = pokemon.types[1].type.name 
+    PokemonInnerHTML = `
+    <div class="pokemon__imageContainer" id="${PokemonName}">
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${PokemonID}.png"/>
+    </div>
+    <div class="pokemon__infomationContainer">
+      <span class="pokemon__id">#${PokemonID.toString().padStart(3, "0")}</span>
+      <h3 class="pokemon__name">${PokemonName}</h3>
+      <small class="pokemon__type">Tipo: <span>${PokemonType1}/${PokemonType2}</span></small><br>
+    </div>`};
   PokemonElement.setAttribute("class", "pokemon_card");
   PokemonElement.innerHTML = PokemonInnerHTML;
   PokemonContainer.appendChild(PokemonElement);
@@ -56,8 +76,14 @@ const getPokemons = async (id) => {
 };
 
 const receivePokemons = async () => {
-  for (let item = 1; item <= PokemonNumber; item++) {
-    await getPokemons(item);
+  if(j < 21){
+    for (let item = 1; item <= 20; item++) {
+      await getPokemons(item);
+    }
+  }else{
+    for (let item = j; item <= j; item++) {
+      await getPokemons(item);
+    }
   }
   createSearchFilter();
 };
